0eea9b43cb8f2587780f716ebf17f31fa9ac0b08
heitor.deve <heitor.developer@outlook.com>
2023-12-28 14:36:15 -0300

    14. Trabalhando com os Services - parte 2

--- Secao-1-introducao/src/courses/courses.service.ts
+++ Secao-1-introducao/src/courses/courses.service.ts
@@ -12,5 +12,36 @@ export class CoursesService {
             tags: ['teste1','teste2','teste3','teste4','teste5']
         }
     ]
+
+    findAll(){
+        return this.courses;
+    }
+
+    findOne(id: number){
+        return this.courses.find(course => course.id === id)
+    }
+
+    create(createCourseDTO: any){
+        this.courses.push(createCourseDTO);
+    }
+
+    update(id: number, updateCourseDTO: any){
+        const existingCourse = this.findOne(id);
+
+        if(existingCourse){
+            const index = this.courses.findIndex(course => course.id === id)
+            this.courses[index] = {
+                id,
+                ... updateCourseDTO
+            }
+        }
+    }
+
+    remove(id: number){
+        const index = this.courses.findIndex(course => course.id === id)
+        if(index>= 0){
+            this.courses.splice(index)
+        }
+    }
 }
 
